56fb2f76fe6f78e16f459a041af07c259d0443ea
prithvisathiya <prithvisathiyamoorth@US-GN-41464.local>
2015-08-20 17:06:49 -0700

    more

--- app/src/main/java/com/example/prithvisathiyamoorth/mazemeup/MainActivity.java
+++ app/src/main/java/com/example/prithvisathiyamoorth/mazemeup/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.prithvisathiyamoorth.mazemeup;
 
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -52,10 +54,11 @@ public class MainActivity extends ActionBarActivity {
         Toast.makeText(this, System.currentTimeMillis()+"", Toast.LENGTH_SHORT).show();
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     public void setAlarm(View v) {
         AlarmManager alarmManager =  (AlarmManager)getSystemService(ALARM_SERVICE);
         Intent myIntent =  new Intent(MainActivity.this, AlarmService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(MainActivity.this, 0, myIntent, 0);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
 
         TimePicker tp = (TimePicker) findViewById(R.id.time);
 
@@ -64,8 +67,8 @@ public class MainActivity extends ActionBarActivity {
         calendar.set(Calendar.HOUR_OF_DAY, tp.getCurrentHour());
         calendar.set(Calendar.MINUTE, tp.getCurrentMinute());
         calendar.set(Calendar.SECOND, 0);
-//        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 15*1000, pendingIntent);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        //alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 15*1000, pendingIntent);
         Toast.makeText(this, calendar.getTime()+"", Toast.LENGTH_SHORT).show();
     }
 }
